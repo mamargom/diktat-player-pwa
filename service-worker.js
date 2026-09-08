c934543cd11bc730e82395d7fb22721bf9fd662f
mmg <117230657+mamargom@users.noreply.github.com>
2026-09-08 21:31:00 +0200

    Renovar caché offline para V24

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "diktat-player-standalone-v23-filename";
+const CACHE = "diktat-player-standalone-v24-editable-name";
 const GENERATED_CACHE = "diktat-generated-files";
 const APP_SCOPE = self.registration.scope;
 const OFFLINE_PAGE = new URL("index.html", APP_SCOPE).href;
