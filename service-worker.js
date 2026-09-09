1a8ef38ea595aaa24335eb5bda0303eea2930187
mmg <117230657+mamargom@users.noreply.github.com>
2026-09-09 13:19:02 +0200

    Refresh cache after interface hotfix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "diktat-player-standalone-v28-interface-language";
+const CACHE = "diktat-player-standalone-v28-interface-hotfix";
 const GENERATED_CACHE = "diktat-generated-files";
 const APP_SCOPE = self.registration.scope;
 const OFFLINE_PAGE = new URL("index.html", APP_SCOPE).href;
