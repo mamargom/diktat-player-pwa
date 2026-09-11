efe7f049915753bb7e05d4520c718d4b7e782490
mmg <117230657+mamargom@users.noreply.github.com>
2026-09-11 15:16:25 +0200

    Refresh cache for spoken punctuation 45.2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "diktat-player-spoken-punctuation-v45-1";
+const CACHE = "diktat-player-spoken-punctuation-v45-2";
 const GENERATED_CACHE = "diktat-generated-files";
 const APP_SCOPE = self.registration.scope;
 const OFFLINE_PAGE = new URL("index.html", APP_SCOPE).href;
